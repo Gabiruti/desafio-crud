f6c1e771ba809f3d9e03e5fb895973045fd4d64c
Gabriel <gabrielrmeneses@outlook.com>
2023-09-29 19:12:47 -0300

    First BackEnd Commit

---
+++ backend/database/migrations/2023_09_29_202921_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 64);
+            $table->foreign('category_fk')->references('id')->on('categories')->onDelete('cascade');
+            $table->float('value', 8, 2);
+            $table->date('due_date');
+            $table->integer('stock');
+            $table->boolean('perishable_product');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product');
+    }
+};
